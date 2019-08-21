0fee60204a8607fba4d7f27873e9ec2019eda5b9
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-08-21 13:53:26 -0700

    i think i understand this more

--- assignments/this.js
+++ assignments/this.js
@@ -11,18 +11,52 @@
 
 // Principle 1
 
-
+function cat(){
+  console.log(this.meow);
+}
+cat();
+const meow = "Thunderclan";
 
 // code example for Window Binding
 
 // Principle 2
 
+// let forestCat = {
+//   name: 'Fire',
+//   clan: 'Thunderclan',
+//   type:  function(){
+//     console.log('My clan is ' + this.clan);
+//   }
+// }
+
+// forestCat.type();
+
 // code example for Implicit Binding
 
 // Principle 3
 
+let forestCat = function(name, clan, type) {
+  this.name = name;
+  this.clan = clan;
+  this.type= type;
+};
+
+let Sandstorm = new forestCat('Sandstorm', 'Thunderclan', 'Warrior');
+console.log(Sandstorm);
+
 // code example for New Binding
 
 // Principle 4
+
+// var type = function(lead1, lead2, lead3){
+//   console.log('My clan is ' + this.clan + ' and I know ' + lead1 + ' , ' + lead2 +  ', and ' + lead3);
+// };
+// let forestCat = {
+//   name: 'Fire',
+//   clan: 'Thunderclan',
+// };
+// var leader = ['Windclan', 'Riverclan', 'Shadowclan']
+// type.call(forestCat, leader[0], leader[1], leader[2]);
+//type.apply(forestCat, leader)
 
 // code example for Explicit Binding
